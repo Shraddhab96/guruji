b055b6e037d7ef8d895499bb5137d83151968627
shraddha <shraddha.19birajdar@gmail.com>
2023-04-30 12:37:04 +0530

    first commit

---
+++ homepage/src/App.js
@@ -0,0 +1,87 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+function App() {
+  return (   
+    <div className="App">
+      
+      <div class ="body">
+      <br></br> 
+    <h1>GURUJI</h1> 
+    <br></br> <br></br> <br></br> <br></br>  
+     <nav>
+      <a href='Home'>Home</a> &nbsp;&nbsp;&nbsp;&nbsp;
+      <a href='call'>Call with Astrologer</a>&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href='live'>Live (coming soon)</a>
+     </nav>
+     <br></br> <br></br> 
+     <div class ="text">
+      <h1>"Astrology For Clarity"</h1>
+      <p> Your name is a vedia Astrologer and has expertise in vastu<br></br> and mantra theraphy</p>
+      <button>Consult Now</button>     
+      </div> 
+      <br></br> <br></br>  
+       <div class="footer">
+      <nav>
+        <a>24*7 customer Support </a> &nbsp;&nbsp;&nbsp;&nbsp;
+        <a> 100% refund it unsatified</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a>private &confidential</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a>verified Astrologer</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a>secure payment</a>
+      </nav>    
+      </div>  
+      <div id="slider">
+        <ul id="silderview">
+          <li><img src='' alt=''></img></li>
+          <li><img src='' alt=''></img></li>
+          </ul>
+      </div>
+      </div>
+      <div class="new">
+        <br></br>
+        <img src="https://media.istockphoto.com/id/1368722560/vector/drawing-cute-sun-cartoon-vector-illustration-in-white-background-vector-illustration.jpg?s=612x612&w=is&k=20&c=0SOEl9VO-RTtgceEDw98J1zHG4HoG3lhzvLEVD3dY3s="height={100} width={100}>
+        </img>
+       <h2>* User Testimonial *</h2>
+       <div class="video">   
+        <video  autoPlay muted loop src="https://media.istockphoto.com/id/1322892924/video/astrology-chart.mp4?s=mp4-640x640-is&k=20&c=upBzSVfJNpsiLo6ZkHIVslhOPSRuYzFSwB58e3M23BA="height={200}width={200}></video>  
+        <video  autoPlay muted loop src="https://media.istockphoto.com/id/1323074672/video/astrology-chart.mp4?s=mp4-640x640-is&k=20&c=Da13LXViq1Fl4ybcEFiOt7PWaoUFnLYewQWK2fLDleM="height={200}width={200}></video> 
+        <video  autoPlay muted loop src="https://media.istockphoto.com/id/1365022769/video/golden-zodiac-wheel-rotation-cancer.mp4?s=mp4-640x640-is&k=20&c=sIh5thIWfmsw30tHUqCqVuB_HrkZLrsSxkjpJgJUY3E="height={200}width={200}></video>              
+       </div>
+      </div>
+      <br></br>
+      <div class ="solution">
+        <h2> # Many Problem one Solution #</h2>
+        <div class="butonleft">
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6SJZnU8HEvtyz0sxXxNkDx4HYIDmqDaaLnA&usqp=CAU'height={300}width={300}></img>
+        <br></br>
+        &nbsp;&nbsp;&nbsp;<button>love related <br></br>Problem</button>&nbsp;&nbsp;&nbsp;
+        <button>Health<br></br>Problem</button><br></br><br></br>
+        &nbsp;&nbsp;&nbsp;<button>Marraige <br></br>Problem</button>&nbsp;&nbsp;&nbsp;<button>Grah <br></br>Shanti</button><br></br><br></br>
+        &nbsp;&nbsp;&nbsp;<button>Job/Business <br></br>Problem</button>&nbsp;&nbsp;&nbsp;<button>child birth<br></br>Problem</button><br></br><br></br>
+        &nbsp;&nbsp;&nbsp;<button> many  <br></br>Problem</button>&nbsp;&nbsp;&nbsp;<button>Husband/wife<br></br>Problem</button><br></br><br></br>
+        &nbsp;&nbsp;&nbsp;<button>equcation <br></br>Problem</button>&nbsp;&nbsp;&nbsp;<button>lucky number<br></br>color</button><br></br><br></br>
+        </div>      
+      </div>
+      <div class ="review">
+      <h2>&nbsp;&nbsp;&nbsp;GURUJI<br></br></h2>
+      <h4>
+      About guruji s2 ti 3 lines what guruji<br></br>
+      do and how it works About Guruji s2 to <br></br>3 lines what guruji do and how it <br></br> 
+      works<br></br><br></br>
+      Trusted and seals<br></br>
+     <button><a href='https://paytm.com/'>paytm</a></button>&nbsp;&nbsp;&nbsp;
+     <button><a href='https://razorpay.com/'>razorpay</a></button><br></br><br></br>
+     <button><a href='https://stripe.com/en-in'>stripe</a></button>
+    </h4>
+        <div class ="inptext">
+          <input type="text" placeholder='Your name'></input><br></br><br></br>
+          <input type="email" placeholder='Email id'></input><br></br><br></br>
+          <input type='textarea' placeholder='Write query'></input><br></br><br></br>
+          <button>submit</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
